2d949bca2515feafde57e505f50a31cc4455bbb8
Tiran Jayasekara <tiran.jayasekara.999@gmail.com>
2025-04-04 22:56:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve index.html for all routes (SPA fix)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
